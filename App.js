7eb5e0988c0ff17e6e23be27bc36a19ec7b44b99
Pallavi Kumari <pallavikumariryz1@gmail.com>
2023-06-27 12:58:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Login from "./components/Login";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { useEffect } from "react";
+import { getUserAuth } from "./actions";
+
+function App(props) {
+  useEffect(() =>{
+    props.getUserAuth();
+  }, []);
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={
+          <>
+          <Header/>
+          <Home/>
+          </>
+          } />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {};
+}
+
+
+const mapDispatchToProps = (dispatch) => ({
+  getUserAuth: () => dispatch(getUserAuth()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
